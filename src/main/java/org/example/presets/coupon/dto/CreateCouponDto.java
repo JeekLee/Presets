007fcddd262876309dd84da86e7738a54032d258
JeekLee <j3kkun@gmail.com>
2024-04-05 19:05:21 +0900

    feat: JDBC Bulk insert, 쿠폰 코드 생성 로직 구현

--- src/main/java/org/example/presets/coupon/dto/CreateCouponDto.java
+++ src/main/java/org/example/presets/coupon/dto/CreateCouponDto.java
@@ -1,11 +1,14 @@
 package org.example.presets.coupon.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class CreateCouponDto {
+    @JsonProperty(value = "coupon_name")
     private final String couponName;
+    @JsonProperty(value = "coupon_quantity")
     private final Long couponQuantity;
 
     @Builder
